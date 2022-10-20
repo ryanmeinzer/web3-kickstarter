3b0ed502b35acb44ac1d356d34efef4d8d647752
Ryan Meinzer <ryanmeinzer@Ryans-MacBook-Pro.local>
2022-10-20 16:19:33 -0700

    first commit with fully built project and newly deployed contract

---
+++ ethereum/deploy.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+const myAccountMnemonic = process.env.MY_ACCOUNT_MNEMONIC
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+const Web3 = require('web3');
+const compiledFactory = require('./build/CampaignFactory.json');
+
+const provider = new HDWalletProvider(
+  `${myAccountMnemonic}`,
+  'https://goerli.infura.io/v3/78389dcce0d54faaa7e109f5111845f0'
+)
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(
+    JSON.parse(compiledFactory.interface)
+  )
+    .deploy({ data: compiledFactory.bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+deploy();
